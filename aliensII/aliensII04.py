0e1118324146e03df0d9427d85644fc381aa135d
hezii <45364706+hezii@users.noreply.github.com>
2018-12-05 12:17:32 -0500

    Update aliensII04.py

--- aliensII/aliensII04.py
+++ aliensII/aliensII04.py
@@ -1,29 +1,29 @@
 #Identifying variables
-l = int(0)
-w = int(0)
-aliensL = False
-aliensT = False
-aliensP = False
+l = int(0) #The value for the landed space aliens question
+w = int(0) #The value for the weeks the space aliens have been on Earth question
+aliensL = False #boolean for while loop 1
+aliensT = False #boolean for while loop 2
+aliensP = False #boolean for while loop 3
 
 
 #Explaining to the user what to do and first question
 print("The space aliens have invaded Earth!")
 print("Every week, each space alien gives birth to a new space alien.")
-print("   ")
+print("   ") #These are used to space out the print lines
 print("You are the chosen one who must tell us...")
-print("    ")
+print("    ") #These are used to space out the print lines
 
-while aliensL == False:
+while aliensL == False: #first while loop
     try:
         print("How many space aliens has landed on Earth?")
         l = int(input("Please give me an answer greater than 0.   "))
-        print("  ")
+        print("  ") #These are used to space out the print lines
 
         if l > 0:
             aliensL = True
             print("You have said that",l,"space alien(s) has landed on Earth.")
             print("Thanks for your choice.")
-            print("   ")
+            print("   ") #These are used to space out the print lines
             aliensT = False
 
         else:
@@ -35,11 +35,11 @@ while aliensL == False:
             print("  ")
 
 
-while aliensT == False:
+while aliensT == False: #second while loop
     try:
         print("Now you must tell me how long has the space alien(s)")
         print("been on Earth for...?")
-        print("   ")
+        print("   ") #These are used to space out the print lines
     
         w = int(input("Please give me an answer greater than 0, no decimals.    "))              
 
@@ -48,7 +48,7 @@ while aliensT == False:
             print("You have said that the alien(s) have been on Earth")
             print("for",w,"week(s).")
             print("Thanks for your choice.")
-            print("   ")
+            print("   ") #These are used to space out the print lines
             
             print("The following is the population per week...")
             for i in range (w):
@@ -63,7 +63,7 @@ while aliensT == False:
     except ValueError:
         print("Hey you typed something that was a decimal or wasn't a number.")
         print("Please try again.")
-        print("  ")
+        print("  ") #These are used to space out the print lines
 
             
 while aliensP == False:
